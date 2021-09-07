6585c3672512537dbda9e53354e5f5211a49ef8b
Matias Garay <maty1173@gmail.com>
2021-09-06 23:37:28 -0300

    segundo commit:intermedio

--- mayoresMenores.php
+++ mayoresMenores.php
@@ -1,8 +1,10 @@
 <?php
-
-
 /*--Dados como datos 100 numeros enteros (aleatorios entre -100 y 100) mostrar cada uno de ellos indicando si es POSITVO o NEGATIVO-- */
 
+$numeros=[];
 
+for($i=1; $i<=100; $i++){
+    $numeros[]= rand(-100,100); /* carga el array con los numeros*/
+} 
 
 ?>
