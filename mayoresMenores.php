a304b7dcb6535d145b32a897c7863a2d1ceedd12
Matias Garay <maty1173@gmail.com>
2021-09-07 00:15:59 -0300

    tercer commit: finalizado

--- mayoresMenores.php
+++ mayoresMenores.php
@@ -7,4 +7,11 @@ for($i=1; $i<=100; $i++){
     $numeros[]= rand(-100,100); /* carga el array con los numeros*/
 } 
 
+foreach($numeros as $valor){
+    if($valor<0){
+        echo "el numero $valor es NEGATIVO <br>";
+    }else echo "el numero $valor es POSITIVO <br>";
+}
+
+
 ?>
